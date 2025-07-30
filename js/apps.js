e748fa16f9738aab0e95301a9090aa622196bede
Alex <110872020+Alsainz1@users.noreply.github.com>
2025-07-29 23:37:18 -0500

    Add files via upload

---
+++ js/apps.js
@@ -0,0 +1,126 @@
+// Here all versions are located, the same as app.js but this time its apps.js because all versions info is located in this one file
+// To add more versions, copy the versions.v1 and replace with versions.v2, and the same with others (it support more than 9 versions)
+
+versions.v1 = {
+  name: "otto",
+  version: "1",
+  date: "2025",
+  folder: "asset-v1/",
+  ambience: "",
+  looptime: 6857,
+  bpm: 140,
+  totalframe: 328,
+  nbpolo: 7,
+  maxrecloop: 24,
+  bonusloopA: false, 
+  bonusendloopA: true,
+  colBck: "#171B27",
+  col0: "#919191",
+  col1: "#6B7585",
+  col2: "#4b4b4b",
+  col3: "#303740",
+  col4: "#232323",
+  animearray: [
+    {
+      name: "1_tomtom",
+      color: "6B7585",
+      uniqsnd: !0,
+    },
+    {
+      name: "2_koukatum",
+      color: "6B7585",
+      uniqsnd: !0,
+    },
+    {
+      name: "3_foubreak",
+      color: "6B7585",
+      uniqsnd: !0,
+    },
+    {
+      name: "4_atlanta",
+      color: "6B7585",
+      uniqsnd: !0,
+    },
+    {
+      name: "5_tiki",
+      color: "6B7585",
+      uniqsnd: !0,
+    },
+    {
+      name: "6_bass",
+      color: "6B7585",
+      uniqsnd: !1,
+    },
+    {
+      name: "7_monk",
+      color: "6B7585",
+      uniqsnd: !0,
+    },
+    {
+      name: "8_sonar",
+      color: "6B7585",
+      uniqsnd: !1,
+    },
+    {
+      name: "9_souffle",
+      color: "6B7585",
+      uniqsnd: !1,
+    },
+    {
+      name: "10_siren",
+      color: "6B7585",
+      uniqsnd: !1,
+    },
+    {
+      name: "11_pegio",
+      color: "6B7585",
+      uniqsnd: !1,
+    },
+    {
+      name: "12_organ",
+      color: "6B7585",
+      uniqsnd: !1,
+    },
+    {
+      name: "13_pizzi",
+      color: "6B7585",
+      uniqsnd: !1,
+    },
+    {
+      name: "14_arpeg",
+      color: "6B7585",
+      uniqsnd: !1,
+    },
+    {
+      name: "15_tromp",
+      color: "6B7585",
+      uniqsnd: !1,
+    },
+    {
+      name: "16_otto",
+      color: "6B7585",
+      uniqsnd: !1,
+    },
+    {
+      name: "17_houhou",
+      color: "6B7585",
+      uniqsnd: !1,
+    },
+    {
+      name: "18_choir",
+      color: "6B7585",
+      uniqsnd: !1,
+    },
+    {
+      name: "19_glass",
+      color: "6B7585",
+      uniqsnd: !1,
+    },
+    {
+      name: "20_riviera",
+      color: "6B7585",
+      uniqsnd: !1,
+    },
+  ],
+  bonusarray: [],
+};
